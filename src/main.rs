1c6e0887d5da5693221c32450d8daab6c1a9cf1f
KevinWolf254 <mbugua.kevinkanyi@gmail.com>
2023-12-30 14:24:13 +0300

    added logging to file

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,10 @@ use actix_web::{ web, App, HttpServer };
 use dao::Database;
 use dotenv::dotenv;
 use handler::user_handler::{ create_user, delete_user, get_user, update_user };
-use slog::{Logger, Drain, o, info};
+use slog::{Logger, Drain, o, info, warn};
 use std::env;
+use std::fs::OpenOptions;
+use std::net::Ipv4Addr;
 use std::sync::{Arc, Mutex};
 
 mod handler;
@@ -12,14 +14,23 @@ mod model;
 mod dao;
 mod error;
 
+const DEFAULT_SERVER_PORT: u16 = 8080;
+const DEFAULT_MAX_CONNECTIONS: u32 = 5;
+const DEFAULT_LOG_PATH: &str = "log/sms_gateway.log";
+
 pub struct AppState<'a> {
     pub connections: Mutex<u32>,
     pub context: Arc<Database<'a>>,
     pub log: Logger
 }
 
-fn configure_log() -> Logger {
-    let decorator = slog_term::TermDecorator::new().build();
+fn configure_log(log_path: String) -> Logger {
+    let file = OpenOptions::new()
+      .create(true)
+      .append(true)
+      .open(log_path)
+      .unwrap();
+    let decorator = slog_term::PlainDecorator::new(file);
     let drain = slog_term::FullFormat::new(decorator).build().fuse();
     let drain = slog_async::Async::new(drain).build().fuse();
     slog::Logger::root(drain, o!())
@@ -28,18 +39,37 @@ fn configure_log() -> Logger {
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     dotenv().ok();
-    let log = configure_log();
-    info!(log, "Loading environment variables...");
+    let log_path = env::var("LOG_PATH").unwrap_or_else(|_| DEFAULT_LOG_PATH.to_string());
 
-    let server_port = env::var("SERVER_PORT").unwrap_or_else(|_| "8080".to_string());
+    let log = configure_log(log_path);
+
+    let server_port = env::var("SERVER_PORT")
+        .unwrap_or_else(|_| {
+            warn!(log, "SERVER_PORT env variable was not provided. Will default to {}", DEFAULT_SERVER_PORT);
+            DEFAULT_SERVER_PORT.to_string()
+        })
+        .parse()
+        .unwrap_or_else(|_| {
+            warn!(log, "SERVER_PORT was not of type u16. Will default to {}", DEFAULT_SERVER_PORT);
+            DEFAULT_SERVER_PORT
+        });
 
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL has not been set!");
-    let max_connections = env::var("MAX_CONNECTIONS")
-        .unwrap_or_else(|_| "5".to_string()) // Default to 5 if not present
-        .parse()
-        .expect("Failed to parse MAX_CONNECTIONS as u32");
 
-    info!(log, "Starting server at http://localhost:{}", server_port);
+    let max_connections = env::var("MAX_CONNECTIONS")
+        .unwrap_or_else(|_| {
+            warn!(log, "MAX_CONNECTIONS env variable was not provided. Will default to {}", DEFAULT_MAX_CONNECTIONS);
+            DEFAULT_MAX_CONNECTIONS.to_string()
+        }) // Default to 5 if not present
+        .parse()
+        .unwrap_or_else(|_| {
+            warn!(log, "MAX_CONNECTIONS was not of type u32. Will default to {}", DEFAULT_MAX_CONNECTIONS);
+            DEFAULT_MAX_CONNECTIONS
+        });
+        // .expect("Failed to parse MAX_CONNECTIONS as u32");
+    let localhost = Ipv4Addr::new(127, 0, 0, 1);
+    
+    info!(log, "Starting server at http://{:?}:{}", localhost, server_port);
 
     let db_context = Database::new(&database_url, max_connections).await;
 
@@ -61,7 +91,7 @@ async fn main() -> std::io::Result<()> {
                     .service(update_user)
                     .service(delete_user)
             )
-    }).bind(format!("127.0.0.1:{}", server_port))
+    }).bind((localhost, server_port))
     .and_then(|result| {
         info!(log, "Successfully started the server");
         Ok(result)
