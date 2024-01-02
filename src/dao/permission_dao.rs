4b70d78d18b544e9d888d6a39307627b3e1430fd
KevinWolf254 <mbugua.kevinkanyi@gmail.com>
2024-01-02 14:13:56 +0300

    refactoring - dao returns sqlx error

--- src/dao/permission_dao.rs
+++ src/dao/permission_dao.rs
@@ -1,9 +1,7 @@
-use std::borrow::Cow;
-
 use chrono::{DateTime, Utc};
 use sqlx::postgres::PgQueryResult;
 
-use crate::{entity::permission::{Permission, CreatePermission}, error::AppError, error::AppErrorType, dto::pagination::PaginatedResult};
+use crate::{entity::permission::{Permission, CreatePermission}, dto::pagination::PaginatedResult};
 
 use super::Table;
 struct CountResult {
@@ -11,38 +9,18 @@ struct CountResult {
 }
 
 impl<'c> Table<'c, Permission> {
-    
-    pub async fn find_by_id(&self, permission_id: &i16) -> Result<Permission, AppError> {
+
+    pub async fn find_by_id(&self, permission_id: &i16) -> Result<Permission, sqlx::Error> {
         sqlx::query_as!(Permission, 
             r#"SELECT * FROM "SMS_GATEWAY_USER"."PERMISSION" WHERE permission_id = $1 "#, permission_id)
             .fetch_one(&*self.pool)
             .await
-            .map_err(|e| {
-                match e {
-                    sqlx::Error::RowNotFound => AppError::new(Some(format!("Permission with id {} could not be found!", permission_id)), None, AppErrorType::NotFoundError),
-                    error  => {
-                        AppError::new(None, Some(error.to_string()), AppErrorType::DBError)
-                    }
-                }
-            })
     }
 
-    pub async fn find_all(&self) -> Result<Vec<Permission>, AppError> {
-        match
-            sqlx::query_as!(Permission, r#"SELECT * FROM "SMS_GATEWAY_USER"."PERMISSION" "#)
+    pub async fn find_all(&self) -> Result<Vec<Permission>, sqlx::Error> {
+        sqlx::query_as!(Permission, r#"SELECT * FROM "SMS_GATEWAY_USER"."PERMISSION" "#)
             .fetch_all(&*self.pool)
             .await
-        {
-            Ok(permissons) => Ok(permissons),        
-            Err(e) => { 
-                match e {
-                    sqlx::Error::RowNotFound => Ok(Vec::with_capacity(0)),
-                    error  => {
-                        Err(AppError::new(None, Some(error.to_string()), AppErrorType::DBError))
-                    }
-                }
-            },
-        }
     }
 
     pub async fn find_paginated(&self, page: i64, page_size: i64) -> Result<PaginatedResult<Permission>, sqlx::Error> {
@@ -73,41 +51,18 @@ impl<'c> Table<'c, Permission> {
             .await        
     }
 
-    pub async fn create(&self, request: &CreatePermission) -> Result<Permission, AppError> {
+    pub async fn create(&self, request: &CreatePermission) -> Result<Permission, sqlx::Error> {
         let now: DateTime<Utc> = Utc::now();
         sqlx::query_as!(Permission, 
             r#"INSERT INTO "SMS_GATEWAY_USER"."PERMISSION" (name, created_at) VALUES ($1, $2) RETURNING * "#, request.name, now)
             .fetch_one(&*self.pool)
             .await
-            .map_err(|error| {
-                match &error {
-                    sqlx::Error::Database(d) => {
-                        match d.code() {
-                            Some(c) => {
-                                if let Cow::Borrowed(code) = c {
-                                    if code.eq("23505") {
-                                        AppError::new(Some("Permission already exists!".to_string()), None, AppErrorType::BadRequestError)
-                                    } else {
-                                        AppError::new(None, Some(error.to_string()), AppErrorType::DBError)
-                                    }
-                                } else {
-                                    AppError::new(None, Some(error.to_string()), AppErrorType::DBError)
-                                }
-                            },
-                            None => AppError::new(None, Some(error.to_string()), AppErrorType::DBError),
-                        }
-                    },
-                    _ => AppError::new(None, Some(error.to_string()), AppErrorType::DBError),
-                }
-            })
-
     }
 
-    pub async fn delete(&self, permission_id: &i16) -> Result<PgQueryResult, AppError> {
+    pub async fn delete(&self, permission_id: &i16) -> Result<PgQueryResult, sqlx::Error> {
         sqlx::query_as!(Permission, 
             r#"DELETE FROM "SMS_GATEWAY_USER"."PERMISSION" WHERE permission_id = $1 "#, permission_id)
             .execute(&*self.pool)
             .await
-            .map_err(|error| AppError::new(None, Some(error.to_string()), AppErrorType::DBError))
     }
 }
--- src/handler/permission_handler.rs
+++ src/handler/permission_handler.rs
@@ -1,6 +1,9 @@
+use std::borrow::Cow;
+
 use actix_web::{ delete, get, post, web::{ Data, Path, ServiceConfig, Query }, HttpResponse };
 use slog::error;
-use crate::{ AppState, entity::permission::CreatePermission, error::{AppError, AppErrorType}, dto::pagination::PaginationRequest };
+use sqlx::Error::RowNotFound;
+use crate::{ AppState, entity::permission::{CreatePermission, Permission}, error::{AppError, AppErrorType}, dto::pagination::PaginationRequest };
 use actix_web_validator::Json;
 
 pub fn init(cfg: &mut ServiceConfig) {
@@ -13,45 +16,91 @@ pub fn init(cfg: &mut ServiceConfig) {
 
 #[get("permissions")]
 pub async fn get_permissions(state: Data<AppState<'_>>) -> Result<HttpResponse , AppError> {
-    state.context.permissions.find_all().await.map(|permissions| HttpResponse::Ok().json(permissions))
-        .map_err(|e| {
-            error!(state.log, "{:?}", e); e
-        })
+    match state.context.permissions.find_all().await {
+        Ok(permissions) => Ok(HttpResponse::Ok().json(permissions)),
+        Err(e) => {
+            error!(state.log, "Error occured: {:?}", e); 
+            match e {
+                sqlx::Error::RowNotFound => {
+                    let empty: Vec<Permission> = Vec::with_capacity(0);
+                    Ok(HttpResponse::Ok().json(empty))
+                },
+                error  => {
+                    Err(AppError::new(None, Some(error.to_string()), AppErrorType::DBError))
+                }
+            }                
+        },
+    }
 }
 
 #[get("permissions-paginated")]
 pub async fn get_permissions_paginated(state: Data<AppState<'_>>, pagination: Query<PaginationRequest>) -> Result<HttpResponse , AppError> {
-    state.context.permissions.find_paginated(pagination.page, pagination.page_size).await.map(|results| HttpResponse::Ok().json(results))
-        .map_err(|e| AppError::new(None, Some(e.to_string()), AppErrorType::DBError))
+    state.context.permissions.find_paginated(pagination.page, pagination.page_size).await
+        .map(|results| HttpResponse::Ok().json(results))
+        .map_err(|e| {
+            error!(state.log, "Error occured: {:?}", e); 
+            AppError::new(None, Some(e.to_string()), AppErrorType::DBError)
+        })
 }
 
 #[get("permissions/{permission_id}")]
 pub async fn get_permission_by_id(state: Data<AppState<'_>>, path: Path<i16>) -> Result<HttpResponse , AppError> {
     let permission_id = path.into_inner();
-    state.context.permissions.find_by_id(&permission_id).await.map(|permission| HttpResponse::Ok().json(permission))
+    state.context.permissions.find_by_id(&permission_id).await
+        .map(|permission| HttpResponse::Ok().json(permission))
         .map_err(|e| {
-            error!(state.log, "{:?}", e); e
+            error!(state.log, "Error occured: {:?}", e); 
+            match e {
+                RowNotFound => AppError::new(Some(format!("Permission with id {} could not be found!", permission_id)), None, AppErrorType::NotFoundError),
+                error  => {
+                    error!(state.log, "Error occured: {:?}", error); 
+                    AppError::new(None, Some(error.to_string()), AppErrorType::DBError)
+                }
+            }
         })
 }
 
 #[post("permissions")]
 pub async fn create_permission(state: Data<AppState<'_>>, body: Json<CreatePermission>) -> Result<HttpResponse , AppError>  {
     state.context.permissions.create(&body.into_inner()).await.map(|permission| HttpResponse::Ok().json(permission))
-        .map_err(|e| {
-            error!(state.log, "{:?}", e); e
+        .map_err(|error| {
+            error!(state.log, "Error occured: {:?}", error); 
+            match &error {
+                sqlx::Error::Database(d) => {
+                    match d.code() {
+                        Some(c) => {
+                            if let Cow::Borrowed(code) = c {
+                                if code.eq("23505") {
+                                    AppError::new(Some("Permission already exists!".to_string()), None, AppErrorType::BadRequestError)
+                                } else {
+                                    AppError::new(None, Some(error.to_string()), AppErrorType::DBError)
+                                }
+                            } else {
+                                AppError::new(None, Some(error.to_string()), AppErrorType::DBError)
+                            }
+                        },
+                        None => AppError::new(None, Some(error.to_string()), AppErrorType::DBError),
+                    }
+                },
+                _ => AppError::new(None, Some(error.to_string()), AppErrorType::DBError),
+            }
         })
 }
 
 #[delete("permissions/{permission_id}")]
 pub async fn delete_permission_with_id(state: Data<AppState<'_>>, path: Path<i16>) -> Result<HttpResponse , AppError> {
     let permission_id = path.into_inner();
-    state.context.permissions.delete(&permission_id).await.map(|result| {
-        if result.rows_affected() == 0 {
-            HttpResponse::NotFound().json(format!("Permission with id {} could not be found!", permission_id))
-        } else {
-            HttpResponse::Ok().body("Permission deleted successfully!")
-        }
-    }).map_err(|e| {
-        error!(state.log, "{:?}", e); e
-    })
+    
+    state.context.permissions.delete(&permission_id).await
+        .map(|result| {
+            if result.rows_affected() == 0 {
+                HttpResponse::NotFound().json(format!("Permission with id {} could not be found!", permission_id))
+            } else {
+                HttpResponse::Ok().body("Permission deleted successfully!")
+            }
+        })
+        .map_err(|error| {
+            error!(state.log, "Error occured: {:?}", error); 
+            AppError::new(None, Some(error.to_string()), AppErrorType::DBError)
+        })
 }
